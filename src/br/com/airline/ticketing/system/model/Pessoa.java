b1461e50a1f5d9b47c91001dcf85169bc66396a0
astonishiing <wcardoso4510@gmail.com>
2025-11-09 15:44:30 -0300

    Criacao da classe Pessoa e seus atributos

---
+++ src/br/com/airline/ticketing/system/model/Pessoa.java
@@ -0,0 +1,9 @@
+package br.com.airline.ticketing.system.model;
+
+import java.util.Date;
+
+public class Pessoa {
+    private Long Id;
+    private String nome, email, telefone, usuario, senha;
+    private Date dataNascimento;
+}
