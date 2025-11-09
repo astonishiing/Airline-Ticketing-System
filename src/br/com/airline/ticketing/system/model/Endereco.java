18d5d8d897c8f0ad1b8251df35fe3d2be3e33ee7
astonishiing <wcardoso4510@gmail.com>
2025-11-09 15:29:14 -0300

    exchange package

---
+++ src/br/com/airline/ticketing/system/model/Endereco.java
@@ -0,0 +1,12 @@
+package br.com.airline.ticketing.system.model;
+
+public class Endereco {
+    private Long id;
+    private String rua;
+    private Integer numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
+}
